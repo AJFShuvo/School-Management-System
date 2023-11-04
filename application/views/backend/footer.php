8888344603d0fa403945ecdf1f316951f86bab79
AJF_Shuvo <ajfshuvo98@gmail.com>
2023-11-05 03:22:13 +0600

    day-39

---
+++ application/views/backend/footer.php
@@ -0,0 +1,5 @@
+<!-- Footer -->
+<footer class="main">
+	&copy; <?php echo date('Y')?> | School Management System in PHP using CodeIgniter Web Framework |
+    <strong>Developed by Abu Jamil Faisal Shuvo, BUBT CSE INTAKE - 48.</strong>
+</footer>
